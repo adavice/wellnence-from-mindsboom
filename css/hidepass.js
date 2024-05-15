63bb5d43eb8a5d7d932d8f41079d7b219762cb83
adavice <97439259+adavice@users.noreply.github.com>
2024-05-15 17:15:19 +0300

    uploaded css files

---
+++ css/hidepass.js
@@ -0,0 +1,8 @@
+function mouseoverPass(obj) {
+  var obj = document.getElementById('myPass');
+  obj.type = "text";
+}
+function mouseoutPass(obj) {
+  var obj = document.getElementById('myPass');
+  obj.type = "password";
+}
